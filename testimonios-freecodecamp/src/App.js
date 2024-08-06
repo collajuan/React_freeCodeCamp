88fd2deae81847f7282fe214e447374ef9fe8e55
collajuan <collajuan@gmail.com>
2024-08-06 13:12:12 +0200

    Testimonio

--- testimonios-freecodecamp/src/App.js
+++ testimonios-freecodecamp/src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import Testimonio from './componentes/Testimonio';
+import Testimonio from './componentes/Testimonio'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className='contenedor-principal'>
+
+      <h1>Esto es el titulooooooooooooooooooooooo</h1>
       <Testimonio />
+      </div>
     </div>
   );
 }
--- testimonios-freecodecamp/src/componentes/Testimonio.js
+++ testimonios-freecodecamp/src/componentes/Testimonio.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import '../stylesheets/Testimonio.css';
+
 function Testimonio () {
   return (
     <div className='contenedor-testimonio'>
@@ -11,7 +13,9 @@ function Testimonio () {
         <div className='contenedor-texto-testimonio'>
           <p className='nombre-testimonio'>Emma</p>
           <p className='cargo-testimonio'>Ingeniera</p>
-          <p className='texto-testimonio'>dfsddfsdfsdfdsf</p>
+          <p className='texto-testimonio'>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+
+</p>
         </div>
     </div>
   )
