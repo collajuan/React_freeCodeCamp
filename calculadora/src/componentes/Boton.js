2bb514d0b982965a828c6b57c358d53e75a6db37
collajuan <collajuan@gmail.com>
2024-08-12 17:09:27 +0200

    botonclear

--- calculadora/src/componentes/Boton.js
+++ calculadora/src/componentes/Boton.js
@@ -9,7 +9,8 @@ function Boton(props) {
 
 
     return (
-        <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}>
+        <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()} 
+        onClick={() => props.manejarClic(props.children)}>
             {/* Valor que tiene la etiqueta en el padre */}
             {props.children}
 
