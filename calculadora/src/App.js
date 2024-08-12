cb8444512b74265f97be91d5eea6bf18b215b0fd
collajuan <collajuan@gmail.com>
2024-08-12 17:14:31 +0200

    calcularResultado

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -3,6 +3,7 @@ import Boton from './componentes/Boton';
 import BotonClear from './componentes/BotonClear';
 import Pantalla from './componentes/Pantalla';
 import { useState } from 'react'
+import { evaluate} from 'mathjs'
 
 function App() {
 
@@ -10,6 +11,10 @@ function App() {
 
   const agregarInput = val => {
     setInput(input + val)
+  }
+
+  const calcularResultado = () => {
+    setInput(evaluate(input))
   }
 
   return (
@@ -36,10 +41,10 @@ function App() {
           <Boton manejarClic={agregarInput}>7</Boton>
           <Boton manejarClic={agregarInput}>8</Boton>
           <Boton manejarClic={agregarInput}>9</Boton>
-          <Boton manejarClic={agregarInput}>x</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
         </div>
         <div className='fila'>
-          <Boton manejarClic={agregarInput}>=</Boton>
+          <Boton manejarClic={calcularResultado}>=</Boton>
           <Boton manejarClic={agregarInput}>0</Boton>
           <Boton manejarClic={agregarInput}>.</Boton>
           <Boton manejarClic={agregarInput}>/</Boton>
