ab851cf294ee1e37efe2e64e197490d1ad8e822e
collajuan <collajuan@gmail.com>
2024-08-12 20:07:16 +0200

    listaDeTareas

--- aplicacion-react-tareas/src/App.js
+++ aplicacion-react-tareas/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import ListaDeTareas from './componentes/ListaDeTareas';
 
 function App() {
   return (
     <div className="aplicacion-tareas">
       <div className='tareas-lista-principal'>
         <h1>Mis tareas</h1>
+        <ListaDeTareas />
       </div>
     </div>
   );
